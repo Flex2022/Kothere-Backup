1d72e955aeb23b9905dbae1f2d83bdc032884c51
AbdelrahmanShahrour <abdalrahman_shahrour@outlook.com>
2024-02-03 19:47:58 +0300

    add modules

---
+++ flex_hr_applicant/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Flex HR Applicant',
+    'version': '1.0',
+    'summary': 'Add some info about HR Applicant',
+    'description': 'Add some info about HR Applicant',
+    'category': 'Human Resources',
+    'author': 'Abdalrahman Shahrour',
+    'website': 'https://www.flex-ops.com',
+    'license': 'AGPL-3',
+    'depends': ['base', 'hr_recruitment'],
+    'data': ['views/hr_applicant_views.xml'],
+    'installable': True,
+    'auto_install': False,
+}
