19f2d98f2d097be7a43569b21caac4965ac366a4
AbdelrahmanShahrour <abdalrahman_shahrour@outlook.com>
2024-02-04 16:06:54 +0300

    add new mod to module called flex_hr_applicant

--- flex_hr_applicant/models/__init__.py
+++ flex_hr_applicant/models/__init__.py
@@ -1,4 +1,8 @@
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class HrApplicant(models.Model):
     _inherit = 'hr.applicant'
@@ -22,6 +26,34 @@ class HrApplicant(models.Model):
     medical_examination_date = fields.Date(string='Medical Examination Date')
     flight_tickets_booking_date = fields.Date(string='Flight Tickets Booking Date')
     quarantine_date = fields.Date(string='Quarantine Date')
+
+    visa_expiry_date = fields.Date(string='Visa Expiry Date')
+    # Date of residence
+    date_of_residence = fields.Date(string='Date of Residence')
+
+    # Nationality
+    nationality = fields.Many2one('res.country', string='Nationality', default='SA')
+
+
+    @api.onchange('visa_expiry_date')
+    def _onchange_visa_status(self):
+        today = fields.Date.today()
+        after_six_month = today + relativedelta(months=6)
+        # print(after_six_month, self.visa_expiry_date)
+        if isinstance(self.visa_expiry_date, date) and isinstance(after_six_month, date):
+            # if self.visa_expiry_date < after_six_month:
+            if self.visa_expiry_date < after_six_month:
+                raise ValidationError('Visa Expiry Date must be more than 6 month')
+
+    @api.onchange('date_of_residence')
+    def _onchange_date_of_residence(self):
+        today = fields.Date.today()
+        after_90_days = today + relativedelta(days=90)
+        if isinstance(self.visa_expiry_date, date) and isinstance(after_90_days, date):
+            if self.date_of_residence < after_90_days:
+                raise ValidationError('Date of Residence must be more than 90 days')
+
+
 
 
 
