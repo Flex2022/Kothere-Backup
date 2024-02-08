ba2151e20746071643c55be4a1d1d2ef584ad405
AbdelrahmanShahrour <abdalrahman_shahrour@outlook.com>
2024-02-08 12:20:00 +0300

    add mod to flex_po_approvals_status

--- flex_po_approvals_status/models/po.py
+++ flex_po_approvals_status/models/po.py
@@ -21,86 +21,86 @@ class PurcheseOrder(models.Model):
     def button_submit_to_po(self):
         self.write({'state': 'po'})
 
-        group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_po_man')
-        # give users if they have group sending_approvers
-        if group_approvers:
-            group_procurement_approvers = group_approvers.users
-
-            # Gather their email addresses
-            email_to = ','.join(user.email for user in group_procurement_approvers)
-            # send email to all users have group sending_approvers
-            self.env['mail.mail'].create({
-                'subject': f'PO manmager Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to PO Manager',
-                'author_id': self.env.user.partner_id.id,
-                'body_html': 'Submitted to PO Manager',
-                'email_to': email_to,
-            }).send()  # sent the email
+        # group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_po_man')
+        # # give users if they have group sending_approvers
+        # if group_approvers:
+        #     group_procurement_approvers = group_approvers.users
+        #
+        #     # Gather their email addresses
+        #     email_to = ','.join(user.email for user in group_procurement_approvers)
+        #     # send email to all users have group sending_approvers
+        #     self.env['mail.mail'].create({
+        #         'subject': f'PO manmager Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to PO Manager',
+        #         'author_id': self.env.user.partner_id.id,
+        #         'body_html': 'Submitted to PO Manager',
+        #         'email_to': email_to,
+        #     }).send()  # sent the email
 
     def button_submit_to_factory(self):
         self.write({'state': 'factory'})
 
-        group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_fa_man')
+        # group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_fa_man')
         # give users if they have group sending_approvers
-        if group_approvers:
-            group_procurement_approvers = group_approvers.users
+        # if group_approvers:
+        #     group_procurement_approvers = group_approvers.users
 
-            # Gather their email addresses
-            email_to = ','.join(user.email for user in group_procurement_approvers)
-            # send email to all users have group sending_approvers
-            self.env['mail.mail'].create({
-                'subject': f'factory manmager Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to factory Manager',
-                'author_id': self.env.user.partner_id.id,
-                'body_html': 'Submitted to factory Manager',
-                'email_to': email_to,
-            }).send()  # sent the email
+        # Gather their email addresses
+        # email_to = ','.join(user.email for user in group_procurement_approvers)
+        # # send email to all users have group sending_approvers
+        # self.env['mail.mail'].create({
+        #     'subject': f'factory manmager Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to factory Manager',
+        #     'author_id': self.env.user.partner_id.id,
+        #     'body_html': 'Submitted to factory Manager',
+        #     'email_to': email_to,
+        # }).send()  # sent the email
 
     def button_reject_to_draft(self):
         self.write({'state': 'draft'})
 
-        self.env['mail.mail'].create({
-            'subject': f'reject from PO manager Send {self.name}',
-            'author_id': self.env.user.partner_id.id,
-            'body_html': 'reject from procurement',
-            'email_to': self.user_id.email,
-        }).send()  # sent the email
+        # self.env['mail.mail'].create({
+        #     'subject': f'reject from PO manager Send {self.name}',
+        #     'author_id': self.env.user.partner_id.id,
+        #     'body_html': 'reject from procurement',
+        #     'email_to': self.user_id.email,
+        # }).send()  # sent the email
 
     def button_submit_to_financial(self):
         self.write({'state': 'factory_aproved'})
 
-        group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_fin_man')
+        # group_approvers = self.env.ref('flex_po_approvals_status.flex_po_approvals_status_access_fin_man')
         # give users if they have group sending_approvers
-        if group_approvers:
-            group_procurement_approvers = group_approvers.users
+        # if group_approvers:
+        #     group_procurement_approvers = group_approvers.users
 
-            # Gather their email addresses
-            email_to = ','.join(user.email for user in group_procurement_approvers)
-            # send email to all users have group sending_approvers
-            self.env['mail.mail'].create({
-                'subject': f'Financial Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to Financial Manager',
-                'author_id': self.env.user.partner_id.id,
-                'body_html': 'Submitted to factory Manager',
-                'email_to': email_to,
-            }).send()  # sent the email
+        # Gather their email addresses
+        # email_to = ','.join(user.email for user in group_procurement_approvers)
+        # send email to all users have group sending_approvers
+        # self.env['mail.mail'].create({
+        #     'subject': f'Financial Approval - Submitted, Hello please check this purchase order {self.name}, needed your approval to send it to Financial Manager',
+        #     'author_id': self.env.user.partner_id.id,
+        #     'body_html': 'Submitted to factory Manager',
+        #     'email_to': email_to,
+        # }).send()  # sent the email
 
     def button_reject_to_po(self):
         self.write({'state': 'po'})
 
-        self.env['mail.mail'].create({
-            'subject': f'reject from factory manager Send {self.name}',
-            'author_id': self.env.user.partner_id.id,
-            'body_html': 'reject from factory',
-            'email_to': self.user_id.email,
-        }).send()  # sent the email
+        # self.env['mail.mail'].create({
+        #     'subject': f'reject from factory manager Send {self.name}',
+        #     'author_id': self.env.user.partner_id.id,
+        #     'body_html': 'reject from factory',
+        #     'email_to': self.user_id.email,
+        # }).send()  # sent the email
 
     def button_reject_to_factory(self):
         self.write({'state': 'factory'})
 
-        self.env['mail.mail'].create({
-            'subject': f'reject from Financial manager Send {self.name}',
-            'author_id': self.env.user.partner_id.id,
-            'body_html': 'reject from Financial',
-            'email_to': self.user_id.email,
-        }).send()  # sent the email
+        # self.env['mail.mail'].create({
+        #     'subject': f'reject from Financial manager Send {self.name}',
+        #     'author_id': self.env.user.partner_id.id,
+        #     'body_html': 'reject from Financial',
+        #     'email_to': self.user_id.email,
+        # }).send()  # sent the email
 
     def button_confirm(self):
         for order in self:
