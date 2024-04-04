5273d6d9befaf88b8955d04a2d7ee9034188423d
sohaib <sohaib@flex-ops.com>
2024-04-04 12:47:37 +0300

    Edit

--- flex_employee_seq_contract/models/hr_employee.py
+++ flex_employee_seq_contract/models/hr_employee.py
@@ -4,7 +4,10 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    employee_number = fields.Char(string='Employee Number', compute='compute_employee_number', store=True)
+    employee_number = fields.Char(string='Employee Number',
+                                  # compute='compute_employee_number',
+                                  # store=True
+                                  )
 
     @api.depends('department_id')
     def compute_employee_number(self):
