c7423c0b65802fc4e8f73f1bed348381f89c58a9
Hossam Zaki <hossamzaki616@gmail.com>
2024-04-08 23:11:00 +0200

    IMP: notification 02

--- flex_hr_app/models/hr_notify.py
+++ flex_hr_app/models/hr_notify.py
@@ -16,4 +16,15 @@ class BaseModel(models.Model):
     model_name = fields.Char(string='Model', readonly=True)
     res_id = fields.Integer(string='Record Id', readonly=True)
     is_read = fields.Boolean(string='Is Read', readonly=True)
-    image_url = fields.Char(string='Image Url', readonly=True)
+    image_url = fields.Char(string='Image Url', compute='_get_image_url')
+
+    _image_url = {
+        "hr.leave": "/hr_holidays/static/description/icon.png",
+        "hr.loan": "/nthub_loan_management/static/description/icon.png",
+        "hr.expense": "/hr_expense/static/description/icon.png",
+    }
+
+    @api.depends('model_name')
+    def _get_image_url(self):
+        for rec in self:
+            rec.image_url = self._image_url.get(rec.model_name, False)
