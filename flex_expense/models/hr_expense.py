36ac2b5d3dd1bf87adbec58de42eee496dcf4947
flexOpsThirdParty <m.hachemi@flex-ops.com>
2024-04-23 06:33:44 +0100

    add flex_expense

--- flex_expense/models/hr_expense.py
+++ flex_expense/models/hr_expense.py
@@ -2,10 +2,10 @@ from odoo import api, fields, Command, models, _
 
 
 class HrExpense(models.Model):
-    _name = "hr.expense"
+    _inherit = "hr.expense"
 
     def _prepare_payments_vals(self):
         res = super(HrExpense, self)._prepare_payments_vals()
-        if self.payment_mode == 'own_account':
+        if self.payment_mode == 'company_account':
             res['partner_id'] = self.company_id.partner_id.id
         return res
