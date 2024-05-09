2b4ff6cfcc2dadeb8ebb4e7bf253d5bb4663f4e1
flexOpsThirdParty <m.hachemi@flex-ops.com>
2024-05-09 14:43:18 +0100

    flex_additions_deductions

--- flex_additions_deductions/models/account_move.py
+++ flex_additions_deductions/models/account_move.py
@@ -125,10 +125,10 @@ class AccountMove(models.Model):
     def _compute_contract_amount(self):
         for record in self:
             record.contract_amount = 0.0
-            if record.invoice_origin:
-                sale_order = self.env['sale.order'].search([('name', '=', record.invoice_origin)], limit=1)
-                if sale_order:
-                    record.contract_amount = float(sale_order.amount_total)
+            # if record.invoice_origin:
+            #     sale_order = self.env['sale.order'].search([('name', '=', record.invoice_origin)], limit=1)
+            #     if sale_order:
+            #         record.contract_amount = float(sale_order.amount_total)
 
     # @api.depends('invoice_origin')
     # def _compute_project_invoice_from_sale_order(self):
