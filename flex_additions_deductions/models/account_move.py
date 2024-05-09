82dd9d4c7361d99dd01a88664d85240fab8948a8
flexOpsThirdParty <m.hachemi@flex-ops.com>
2024-05-09 14:41:56 +0100

    flex_additions_deductions

--- flex_additions_deductions/models/account_move.py
+++ flex_additions_deductions/models/account_move.py
@@ -48,7 +48,7 @@ class AccountMove(models.Model):
 
     is_out_invoice = fields.Boolean('Is Out Invoice', compute='_compute_is_out_invoice', store=False)
 
-    contract_amount = fields.Monetary(string='Contract Amount', compute='_compute_contract_amount', store=False)
+    contract_amount = fields.Float(string='Contract Amount', compute='_compute_contract_amount', store=False)
 
     project_manager = fields.Many2one('res.partner', string='Project Manager',
                                       compute='_compute_project_manager', store=True)
@@ -128,7 +128,7 @@ class AccountMove(models.Model):
             if record.invoice_origin:
                 sale_order = self.env['sale.order'].search([('name', '=', record.invoice_origin)], limit=1)
                 if sale_order:
-                    record.contract_amount = sale_order.amount_total
+                    record.contract_amount = float(sale_order.amount_total)
 
     # @api.depends('invoice_origin')
     # def _compute_project_invoice_from_sale_order(self):
