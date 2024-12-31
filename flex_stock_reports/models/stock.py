0ccea2732603596480df2a3f18f48b0a77a447fc
Hossam Zaki <hossamzaki616@gmail.com>
2024-12-31 20:55:11 +0200

    upd: driver name

--- flex_stock_reports/models/stock.py
+++ flex_stock_reports/models/stock.py
@@ -4,6 +4,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     receipt_permission = fields.Char(string='Warehouse Receipt Permission')
-    driver_name = fields.Many2one('res.partner', string='Driver Name')
+    # driver_name = fields.Many2one('res.partner', string='Driver Name')
+    driver_name = fields.Char(string='Driver Name')
     car_number = fields.Char(string='Car Number')
     receipt_no = fields.Char(string='Receipt No')
