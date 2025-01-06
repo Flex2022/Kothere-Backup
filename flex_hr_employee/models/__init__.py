7a614204b98b6d593bd04f05b672d90b0d572d2c
Hossam Zaki <hossamzaki616@gmail.com>
2025-01-06 18:39:00 +0200

    extra employee fields

--- flex_hr_employee/models/__init__.py
+++ flex_hr_employee/models/__init__.py
@@ -7,3 +7,9 @@ class NewModule(models.Model):
     mcj = fields.Char(string="MCJ")
     iqama_id = fields.Char(string="Iqama ID")
     end_of_iqama = fields.Date(string="End of Iqama")
+
+    type_of_insurance = fields.Char(string="Type Of Insurance")
+    bank_name = fields.Char(string="Bank")
+    bank_number = fields.Char(string="Bank Number")
+    identification_expiry = fields.Date(string="Identification Expiry")
+    passport_expiry = fields.Date(string="Passport Expiry")
