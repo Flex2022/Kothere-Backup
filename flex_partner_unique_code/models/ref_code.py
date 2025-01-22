2a3296cd1165483db539f1723fbc453d0c3118ed
Hossam Zaki <hossamzaki616@gmail.com>
2025-01-22 19:09:25 +0200

    partner arabic name

--- flex_partner_unique_code/models/ref_code.py
+++ flex_partner_unique_code/models/ref_code.py
@@ -1,11 +1,22 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    arabic_name = fields.Char(string='Arabic Name')
     partner_code = fields.Char(string="Partner Code", required=False, copy=False, readonly=True,
                            default=lambda self: _('New'))
+
+    @api.depends('complete_name', 'email', 'vat', 'state_id', 'country_id', 'commercial_company_name')
+    @api.depends_context('show_address', 'partner_show_db_id', 'address_inline', 'show_email', 'show_vat', 'lang')
+    def _compute_display_name(self):
+        if self.env.lang == 'ar_001':
+            for partner in self:
+                partner.display_name = (partner.arabic_name or partner.with_context(lang=self.env.lang)._get_complete_name()).strip()
+        else:
+            super(ResPartner, self)._compute_display_name()
 
     @api.model_create_multi
     def create(self, vals_list):
