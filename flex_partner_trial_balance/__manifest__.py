1c0793e41d1ee3385a1d7eb74c147a953e5ccba2
Hossam Zaki <hossamzaki616@gmail.com>
2025-02-04 17:19:56 +0200

    add: flex_partner_trial_balance

---
+++ flex_partner_trial_balance/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner Trial Balance',
+    'version': '1.0',
+    'description': """""",
+    'summary': '',
+    'author': "Hossam Zaki, Flex-Ops",
+    'website': "https://flex-ops.com",
+    'depends': [
+        'account_reports',
+    ],
+    'data': [
+        'views/menus.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ flex_partner_trial_balance/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# from . import _account_partner_trial_balance_report
+from . import partner_ledger_handler_custom
+from . import account_partner_trial_balance_report
