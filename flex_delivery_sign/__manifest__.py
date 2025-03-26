096475b2e098f155ca96b2fec28aed38e39d09b9
sohaibflex <sohaib@flex-ops.com>
2025-03-26 16:17:37 +0300

    Add Sign

---
+++ flex_delivery_sign/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Flex Delivery Sign',
+    'version': '1.0',
+    'summary': '',
+    'description': '',
+    'category': '',
+    'author': '',
+    'website': '',
+    'license': '',
+    'depends': ['stock'],
+    'data': ['reports/slip.xml'],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+
+}
