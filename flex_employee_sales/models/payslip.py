04157ab1641af0cb8004da7e3250186aeb0f6014
sohaibflex <sohaib@flex-ops.com>
2025-04-09 12:42:30 +0300

    Add Group by sale employee in sale analasiys

--- flex_employee_sales/models/payslip.py
+++ flex_employee_sales/models/payslip.py
@@ -5,6 +5,7 @@ class Payslip(models.Model):
     _inherit = 'hr.payslip'
 
     total_sales = fields.Float(string='Total Sales Employee', compute='_compute_total_sales', store=True)
+    total_sale_employee_quantity = fields.Float(string='Total Sale Employee Quantity', compute='_compute_total_sales', store=True)
     total_all_sales = fields.Float(string='Total Sales', compute='_compute_all_total_sales', store=True)
     total_sale_quantity = fields.Float(string='Total Sale Quantity', compute='_compute_all_total_sales', store=True)
 
@@ -26,6 +27,14 @@ class Payslip(models.Model):
                     total_sales += invoice.amount_total
             payslip.total_sales = total_sales
 
+            total_sale_quantity = 0
+            if all_invoice_for_employee:
+                for invoice in all_invoice_for_employee:
+                    lines = invoice.invoice_line_ids
+                    for line in lines:
+                        total_sale_quantity += line.quantity
+            payslip.total_sale_employee_quantity = total_sale_quantity
+
 
     def update_sale_total(self):
         for payslip in self:
@@ -40,7 +49,7 @@ class Payslip(models.Model):
 
             all_invoice_based_on_date_range = self.env['account.move'].search(
                 [
-                    ('sale_employee_id', '=', payslip.employee_id.id),
+                    # ('sale_employee_id', '=', payslip.employee_id.id),
                     ('state', '=', 'posted'),
                     ('invoice_date', '>=', payslip.date_from),
                     ('invoice_date', '<=', payslip.date_to)
@@ -53,6 +62,15 @@ class Payslip(models.Model):
                 for invoice in all_invoice_for_employee:
                     total_sales += invoice.amount_total
             payslip.total_sales = total_sales
+
+            total_sale_employee_quantity = 0
+            if all_invoice_for_employee:
+                for invoice in all_invoice_for_employee:
+                    lines = invoice.invoice_line_ids
+                    for line in lines:
+                        total_sale_employee_quantity += line.quantity
+            payslip.total_sale_employee_quantity = total_sale_employee_quantity
+
             total_sales_2 = 0
             if all_invoice_based_on_date_range:
                 for invoice in all_invoice_based_on_date_range:
@@ -75,7 +93,7 @@ class Payslip(models.Model):
         for payslip in self:
             all_invoice_for_employee = self.env['account.move'].search(
                 [
-                    ('sale_employee_id', '=', payslip.employee_id.id),
+                    # ('sale_employee_id', '=', payslip.employee_id.id),
                     ('state', '=', 'posted'),
                     ('invoice_date', '>=', payslip.date_from),
                     ('invoice_date', '<=', payslip.date_to)
