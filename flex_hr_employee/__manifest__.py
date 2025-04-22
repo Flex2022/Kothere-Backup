e75fd69a956cef2117b66176578fe8c85c162779
sohaibflex <sohaib@flex-ops.com>
2025-04-22 14:30:39 +0300

    Add Hr Responsable in email

--- flex_hr_employee/__manifest__.py
+++ flex_hr_employee/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Abdalrahman Shahrour',
     'website': 'https://flex-ops.com',
     'license': 'AGPL-3',
-    'depends': ['hr','mail'],
+    'depends': ['hr','mail','hr_contract'],
     'data': [
         'data/ir_corn.xml',
         'views/hr_employee.xml'
